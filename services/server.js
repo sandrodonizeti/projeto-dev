3ab8d5505f9d44313733abc8873cc95349a0c561
sandrodonizeti <sandrogomesoficial@gmail.comcd>
2024-09-20 16:11:14 -0300

    JJ

--- services/server.js
+++ services/server.js
@@ -1,14 +1,10 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 const port = 3000;
 
-mongoose.connect('mongodb://localhost:27017/photo_upload_db', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+
 
 app.use(express.json());
 app.use('/user', userRoutes);
