bc2e74b2db01432647fb0342a2ae9193b5ddd72e
sandrodonizeti <sandrogomesoficial@gmail.comcd>
2024-09-20 02:57:15 -0300

    autss

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
+const indexRoutes = require('./routes/index');
 const userRoutes = require('./routes/userRoutes');
-const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
@@ -20,22 +19,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', indexRoutes);
 app.use('/usuarios', userRoutes);
-app.use('/autenticacao', authRoutes);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+
   res.status(err.status || 500);
   res.render('error');
 });
