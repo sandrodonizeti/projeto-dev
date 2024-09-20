642e22485ff94b176abc936db81c2f1b15d309ee
sandrodonizeti <sandrogomesoficial@gmail.comcd>
2024-09-19 22:57:35 -0300

    inicio1

--- app.js
+++ app.js
@@ -3,91 +3,41 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const multer = require('multer');
-const session = require('express-session');
 
-const checkLoggedIn = require('./middlewares/checkLoggedIn');
 const indexRouter = require('./routes/index');
-const userRoutes = require('./routes/userRoutes');
-const authRoutes = require('./routes/authRoutes');
-const uploadRoutes = require('./routes/uploadRoutes');
+const usersRouter = require('./routes/users');
+const funcionarioRouter = require('./routes/funcionario.routes');
 
 const app = express();
+
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
-
-
-app.use(express.static(path.join(__dirname, 'uploads')));
-
-
-
-
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    console.log("filename app.js");   
-    cb(null, 'uploads/');
-  },
-  filename: function (req, file, cb) { 
-    console.log("filename app.js");   
-    cb(null, file.originalname);
-  },
-});
-
-
-
-
-const upload = multer({ storage: storage });
-require('dotenv').config();
-app.use(session({
-  secret: process.env.session_secret,
-  resave: false,
-  saveUninitialized: true,
-}));
-
-
-app.use(checkLoggedIn);
 app.use('/', indexRouter);
-app.use('/usuarios', userRoutes);
-app.use('/autenticacao', authRoutes);
-app.post('/upload', upload.single('file'), function (req, res, next) {
-  if (!req.file) {
-    return res.status(400).send('No file selected.');
-  }
-  return res.redirect('/pratica');
-});
-app.post('/upload', upload.single('file'), function (req, res) {
-  if (req.file) {
-    let fileContent = base64_encode(req.file.filename);
-    db.fileExample.create(
-      { fileName: req.file.originalname, fileExt: req.file.mimetype, file: fileContent }
-    ).then(function () {
-      console.log('Success!');
-    }, function (error) {
-      console.log(error); res.sendStatus(500);
-    });
-  }
-  return res.redirect('/pratica');
-});
-app.use(function (req, res, next) {
+app.use('/users', usersRouter);
+app.use('/funcionarios', funcionarioRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
   next(createError(404));
 });
-app.use(function (err, req, res, next) {
-  res.locals.error = {
-    message: err.message,
-    status: err.status || 500,
-    stack: req.app.get('env') === 'development' ? err.stack : '',
-  };
 
-  res.status(res.locals.error.status);
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
   res.render('error');
 });
+
 module.exports = app;
